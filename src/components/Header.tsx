5e7c3ec5869854fb6a77bb16902cb67302a768f6
Rahul <rahul.choudhary@gmail.com>
2025-07-14 23:49:54 +0530

    feat: sidebar collapse

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,33 +3,52 @@ import { FcMoneyTransfer } from "react-icons/fc";
 import { AddExpenseDrawer } from "./Drawer";
 import Logout from "./Logout";
 import { ThemeToggle } from "./ThemeToggle";
+import { SidebarTrigger, useSidebar } from "./ui/sidebar";
+import { Frame, Menu, X } from "lucide-react";
+import { Button } from "./ui/button";
 const Header = () => {
-  return (
-    <header className="w-full  border-gray-300  px-6 py-1 flex items-center justify-between">
-      <div className="hidden md:flex md:items-center gap-4">
-        <div>
-          <FcMoneyTransfer size={30} />
-        </div>
+  const { setOpen, open } = useSidebar();
 
-        <div className="hidden md:flex md:flex-col">
-          <div>
-            <h1 className="bg-gradient-to-r from-blue-600 to-indigo-400 dark:text-white inline-block text-transparent bg-clip-text font-semibold text-xl">
-              Expense Dashboard
-            </h1>
+  return (
+    <>
+      <div className="flex items-center justify-between">
+        <Button
+          onClick={() => setOpen(!open)}
+          className="p-2 rounded-md hover:bg-gray-100 dark:hover:bg-gray-800 transition"
+        >
+          {!open ? (
+            <Menu size={24} className="text-black dark:text-white" />
+          ) : (
+            <X size={24} className="text-black dark:text-white" />
+          )}
+        </Button>
+        <header className="w-full  border-gray-300  px-6 py-1 flex items-center justify-between">
+          <div className="hidden md:flex md:items-center gap-4">
+            <div>
+              <FcMoneyTransfer size={30} />
+            </div>
+
+            <div className="hidden md:flex md:flex-col">
+              <div>
+                <h1 className="bg-gradient-to-r from-blue-600 to-indigo-400 dark:text-white inline-block text-transparent bg-clip-text font-semibold text-xl">
+                  Expense Dashboard
+                </h1>
+              </div>
+              <div>
+                <h1 className="text-sm text-muted font-light text-left">
+                  Track and manage your expenses
+                </h1>
+              </div>
+            </div>
           </div>
-          <div>
-            <h1 className="text-sm text-muted font-light text-left">
-              Track and manage your expenses
-            </h1>
+          <div className="hidden md:flex md:gap-4 items-center">
+            <AddExpenseDrawer />
+            <Logout />
+            <ThemeToggle />
           </div>
-        </div>
+        </header>
       </div>
-      <div className="hidden md:flex md:gap-4 items-center">
-        <AddExpenseDrawer />
-        <Logout />
-        <ThemeToggle />
-      </div>
-    </header>
+    </>
   );
 };
 
--- src/components/layouts/MainLayout.tsx
+++ src/components/layouts/MainLayout.tsx
@@ -4,22 +4,38 @@ import React from "react";
 import Header from "../Header";
 import { AppSidebar } from "../Sidebar";
 import withAuth from "@/hoc/withAuth";
+import { SidebarProvider, useSidebar } from "../ui/sidebar";
 
 const MainLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="flex dark:bg-gray-900 min-h-screen bg-primary">
-      <aside className="w-64  border-r shadow-md">
+    <div className="flex dark:bg-gray-900 min-h-screen ">
+      <SidebarProvider>
+        <LayoutBody>{children}</LayoutBody>
+      </SidebarProvider>
+    </div>
+  );
+};
+
+const LayoutBody = ({ children }: { children: React.ReactNode }) => {
+  const { open } = useSidebar();
+
+  return (
+    <>
+      <aside
+        className={`transition-all duration-300 ${
+          open ? "w-64" : "w-10 "
+        } border-r shadow-md`}
+      >
         <AppSidebar />
       </aside>
-
       <main className="flex-1 flex flex-col">
         <header className="w-full border-b px-6 py-4 shadow-sm">
           <Header />
         </header>
 
-        <section className="flex-1 p-6 bg-gray-50">{children}</section>
+        <section className="flex-1 p-6 bg-gray-50 ">{children}</section>
       </main>
-    </div>
+    </>
   );
 };
 
