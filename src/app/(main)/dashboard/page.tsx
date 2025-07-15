70f8b7514a2b66e71d5ede4df31d1fa7c25ff33a
Rahul <rahul.choudhary@gmail.com>
2025-07-15 22:55:48 +0530

    feat: edit user profile

--- src/app/(main)/dashboard/page.tsx
+++ src/app/(main)/dashboard/page.tsx
@@ -41,7 +41,7 @@ const ExpenseDashBoard = () => {
   return (
     <div>
       <SummaryCards data={summaryData} isLoading={isSummaryLoading} />
-      <div className="bg-gray shadow-sm p-4 mt-4 rounded-3xl dark:bg-gray-900 ">
+      <div className="bg-gray border-[1px] shadow-gray-300 shadow-sm p-4 mt-4 rounded-3xl dark:bg-gray-900 ">
         <ExpenseDataTable
           count={tableData.count}
           data={tableData.data}
---
+++ src/validations/profile.validation.ts
@@ -0,0 +1,19 @@
+import * as Yup from "yup";
+
+export const profileValidationSchema = Yup.object().shape({
+  firstName: Yup.string()
+    .trim()
+    .min(2, "First name must be at least 2 characters")
+    .max(50, "First name cannot exceed 50 characters")
+    .required("First name is required"),
+
+  lastName: Yup.string()
+    .trim()
+    .min(2, "Last name must be at least 2 characters")
+    .max(50, "Last name cannot exceed 50 characters")
+    .required("Last name is required"),
+
+  email: Yup.string()
+    .email("Enter a valid email")
+    .required("Email is required"),
+});
